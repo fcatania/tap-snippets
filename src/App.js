80a33f1c4895003d9bcbe39557745f720eb18215
Felipe Catania <felicatania@gmail.com>
2017-12-28 11:48:50 -0800

    Created Snippet component, with basic styling.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 import SideBar from './SideBar.js';
+import Snippet from './Snippet.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentSnippet: {title: 'Some Title', linkToDocs: 'https://reactjs.org', snippetBody: 'var culo = sucio;'}
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <SideBar />
+        <Snippet snippet={this.state.currentSnippet}/>
       </div>
     );
   }
