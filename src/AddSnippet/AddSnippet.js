370b9a9ec79cb59a5e986627d485d98aa6be00fe
Santiago Iraola <iraola.santi@gmail.com>
2017-12-30 11:23:36 -0800

    unable to add a snippet is any of the fields is empty.

--- src/AddSnippet/AddSnippet.js
+++ src/AddSnippet/AddSnippet.js
@@ -3,6 +3,29 @@ import TextAdder from './TextAdder';
 import './addSnippet.css';
 import 'whatwg-fetch';
 
+let _isSnippetValid = (sentSnippet) => {
+
+  let testObj = {
+    title: 'someething',
+    snippetBody: 'a body',
+    linkdocs: 'www.google.com',
+    tags: [],
+    tips: []
+  }
+  
+  if(Object.keys(testObj).length !== Object.keys(sentSnippet).length) {
+    return false;
+  }
+
+  for(let key in testObj) {
+    if(sentSnippet[key] === undefined || sentSnippet[key].length === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
 class AddSnippet extends Component {
   constructor(props) {
     super(props);
@@ -32,20 +55,24 @@ class AddSnippet extends Component {
     newSnippet.snippetBody = this.snippetNode.value;
     newSnippet.tips = this.state.tips;
     newSnippet.tags = this.state.tags;
-    fetch('http://127.0.0.1:8080/snippets/react', {
-                method: 'POST',
-                headers : {
-                  'Content-Type': 'application/json'
-                },
-                body:JSON.stringify(newSnippet)
-            }).then((res) => res.json())
-            .then((data) => {
-              this.setState({
-                tips: [],
-                tags: []
-              });
-            })
-            .catch((err)=>console.log(err));
+    if(_isSnippetValid(newSnippet)) {
+      fetch('http://127.0.0.1:8080/snippets/react', {
+        method: 'POST',
+        headers : {
+          'Content-Type': 'application/json'
+        },
+        body:JSON.stringify(newSnippet)
+      }).then((res) => res.json())
+      .then((data) => {
+        this.setState({
+          tips: [],
+          tags: []
+        });
+      })
+      .catch((err)=>console.log(err));
+    } else {
+      window.alert('Not a valid snippet. Please complete all fields.');
+    }
   }
 
   render() {
